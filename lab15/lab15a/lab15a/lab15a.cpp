e99339d4054a851a687a3e78a9ef19ddd7c54877
Ekaterina <61124818+katerinacher@users.noreply.github.com>
2023-04-13 01:10:13 +0300

    Add files via upload

---
+++ lab15/lab15a/lab15a/lab15a.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+int main() {
+  setlocale(0, "russian");
+  FILE* f;
+  FILE* g;
+  char filename[10];
+  char word[40];
+  char consonant[] = "qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM", *cletter;
+  char vowel[] = "eyuioaEYUIOA", * vletter;
+  int count = 0;
+
+  printf("Введите имя файла:");
+  gets_s(filename);
+  fopen_s(&f, filename, "w+t");
+  fopen_s(&g, "name", "w+t");
+
+  if (f != NULL) {
+    printf("Введите слова:\n");
+    while (gets_s(word), strlen(word) != 0) {
+      fputs(word, f);
+      fputs("\n", f);
+      if (strlen(word) > 6) {
+        count++;
+      }
+      for (cletter = consonant; *cletter; cletter++) {
+        if (*word == *cletter) {
+          fputs(word, g);
+          fputs("\n", g);
+        }
+      }
+    }
+
+    rewind(f);
+    printf("Сформированный файл:\n");
+    while (fgets(word, 40, f) != NULL) {
+      printf("%s", word);
+    }
+
+    rewind(f);
+    printf("\nСлова, начинающиеся на гласную:\n");
+    while (fgets(word, 40, f) != NULL) {
+      for (vletter = vowel; *vletter; vletter++) {
+        if (*word == *vletter) {
+          printf("%s", word);
+        }
+      }
+    }
+    printf("\nКоличество слов, длина которых больше 6: %d", count);
+    if (count != 0) {
+      printf("\nЭти слова:\n");
+      rewind(f);
+      while (fgets(word, 40, f) != NULL) {
+        if (strlen(word) > 7) {
+          printf("%s", word);
+        }
+      }
+    }
+
+    rewind(g);
+    puts("\nПолученный файл:");
+    while (fgets(word, 40, g) != NULL) {
+      printf("%s", word);
+    }
+
+    _fcloseall();
+  } else {
+    printf("Невозможно открыть файл\n");
+  }
+
+  return 0;
+}
---
+++ lab15/lab15b/lab15b/lab15b.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+int main() {
+  setlocale(0, "russian");
+  FILE* f, * g;
+  char filename[10];
+  int num;
+
+  printf("Введите имя файла:");
+  gets_s(filename);
+  fopen_s(&f, filename, "w+b");
+
+  if (f != NULL) {
+
+    srand((unsigned)time(NULL));
+    int n;
+    printf("Введите количество чисел: ");
+    scanf_s("%d", &n);
+
+    if (n > 0) {
+      for (int i = 0; i < n; i++) {
+        int num = rand() % 100;
+        fwrite(&num, sizeof(int), 1, f);
+      }
+
+      rewind(f);
+      printf("Исходный файл:\n");
+      while (fread(&num, sizeof(int), 1, f) != 0) {
+        printf("%d   ", num);
+      }
+
+      rewind(f);
+      printf("\n\nЧётные цифры: ");
+      while (fread(&num, sizeof(int), 1, f) != 0) {
+        if (num % 2 == 0) {
+          printf("%d   ", num);
+        }
+      }
+      printf("\n");
+
+      fopen_s(&g, "name", "w+b");
+      fseek(f, 0, SEEK_END);
+      int point = ftell(f);
+      int i = sizeof(int);
+
+      while (i <= point) {
+        fseek(f, -i, SEEK_END);
+        fread(&num, sizeof(int), 1, f);
+        if (num % 2 != 0) {
+          fwrite(&num, sizeof(int), 1, g);
+        }
+        i += sizeof(int);
+      }
+
+      rewind(g);
+      printf("\nПолученный файл:\n");
+      while (fread(&num, sizeof(int), 1, g) != 0) {
+        printf("%d   ", num);
+      }
+
+      _fcloseall();
+    } else {
+      printf("Вы ввели некорректное количество чисел\n");
+    }
+  } else {
+    printf("Невозможно открыть файл\n");
+  }
+
+  return 0;
+}
